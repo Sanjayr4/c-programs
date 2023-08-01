69d66c942bbb59a3fc730d7f18a9bf11efb3b2a0
Sanjay.R <133894028+Sanjayr4@users.noreply.github.com>
2023-08-01 19:39:19 +0530

    Add files via upload

---
+++ 1st & last digit of a number .c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n; // n= number
+    printf ("Enter the number ");
+    scanf ("%d",&n);
+    int last,first;
+    printf ("The last digit of the given number is = %d\n",n%10);
+    while (n>=100)
+    {
+        n/=10;
+    }
+    printf ("The 1st digit of the given number is = %d",n);
+
+    return 0;
+}
